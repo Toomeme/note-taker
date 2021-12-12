31fe278c2954ebee18c33aa95b4e24ba218f468c
Jake Toomey <jtoom238@gmail.com>
2021-12-12 11:25:50 -0500

    fix routes, add uuid

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,47 +1,56 @@
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = app => {
 
-    // Setup notes variable
+    // initialize notes variable
     fs.readFile("db/db.json","utf8", (err, data) => {
 
         if (err) throw err;
 
         var notes = JSON.parse(data);
     
-        // initialize the /api/notes get route
-        app.get("/api/notes", function(req, res) {
-            // Read the db file and return all saved notes as object.
+        // initialize the notes get route
+        app.get("/api/notes", (req, res) => {
             res.json(notes);
         });
 
-        // initialize the /api/notes post route
-        app.post("/api/notes", function(req, res) {
-            // Receives a new note, adds it to db.json, then returns the new note
+        // initialize the notes post route
+        app.post("/api/notes", (req, res) => {
             let newNote = req.body;
+            newNote.id = uuidv4();
             notes.push(newNote);
             updateDb();
+            res.json(notes);
             return console.log("Added new note: "+newNote.title);
+            
         });
 
-        // get a note with specific id
-        app.get("/api/notes/:id", function(req,res) {
+        // Retrieves a note with specific id
+        app.get("/api/notes/:id", (req,res) => {
             // display json for the notes array indices of the provided id
             res.json(notes[req.params.id]);
+            console.log("got note with id "+req.params.id);
         });
 
-        // Display notes when /notes is accessed
-        app.get('/notes', function(req,res) {
+        // Deletes a note with specific id
+        app.delete("/api/notes/:id", (req, res) => {
+            notes.splice(req.params.id, 1);
+            updateDb();
+            res.json(notes);
+            console.log("Deleted note with id "+req.params.id);
+        });
+
+        app.get('/notes', (req,res) => {
             res.sendFile(path.join(__dirname, "../public/notes.html"));
         });
         
-        // Display index when all other routes are accessed
-        app.get('*', function(req,res) {
+        app.get('*', (req,res) => {
             res.sendFile(path.join(__dirname, "../public/index.html"));
         });
 
-        //updates the json file whenever a note is added
+        //updates the json file whenever a note is added or deleted
         function updateDb() {
             fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
                 if (err) throw err;
--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,21 @@
-// Require Dependencies
+// require dependencies
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
-
+// initialize express app
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+// initialize data parsing
 app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/routes')(app);
 
-
-
+// initialize listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
